f89c60fda2d3900be393f4eab5bb090ee39f0f34
unknown <matt.mcquain@gmail.com>
2022-12-29 17:38:43 -0800

    still working on 401 deploy error

--- client/script.js
+++ client/script.js
@@ -98,7 +98,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch the response from the server -> get bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatbot-sbhu.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
